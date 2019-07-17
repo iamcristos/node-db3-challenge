2d671347ac8a8142a83dd1eb4cb568c3d9927725
Nmeregini Vincent <nmereginivincent@gmail.com>
2019-07-17 15:35:33 +0100

    -answered all queries in queries.md

--- server.js
+++ server.js
@@ -1,10 +1,16 @@
 const express = require('express');
+const helmet = require('helmet');
+const logger = require('morgan');
 
 const SchemeRouter = require('./schemes/scheme-router.js');
 
 const server = express();
 
+server.use(helmet());
+server.use(logger('dev'));
+
 server.use(express.json());
+
 server.use('/api/schemes', SchemeRouter);
 
 module.exports = server;
